79ff38b330de3f77c537e92b0bd3b532f2d0ad9d
Mohit <artbirds.co.in@gmail.com>
2017-03-11 23:03:54 +0530

    Commit

--- app/src/main/java/com/example/chintu/sellerhub/Listing_Instock_fragment.java
+++ app/src/main/java/com/example/chintu/sellerhub/Listing_Instock_fragment.java
@@ -50,7 +50,7 @@ public class Listing_Instock_fragment extends Fragment implements View.OnClickLi
         recyclerView = (RecyclerView) v.findViewById(R.id.recycler_view_artwork);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-       // loadMalls("http://mohit.hol.es/HeadLines/fetchNews.php");
+        loadMalls("http://mohit.hol.es/HeadLines/fetchNews.php");
         return v;
     }
 
