d204df64d61a8eeba1fc31487938e2bca2d24a39
Mohit <artbirds.co.in@gmail.com>
2017-03-26 00:24:40 +0530

    Commit

---
+++ app/src/main/java/com/example/chintu/sellerhub/Config.java
@@ -0,0 +1,31 @@
+package com.example.chintu.sellerhub;
+
+/**
+ * Created by hp lap on 25-03-2017.
+ */
+
+public class Config {
+    public static final String DATA_URL = "http://artbirdz.hol.es/Artbirdz%20seller%20hub/Listing_View_Product.php?id=";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_CATEGORY = "category";
+    public static final String KEY_STYLE = "style";
+    public static final String KEY_SIZE = "size";
+    public static final String KEY_SURFACE = "surface";
+
+    public static final String KEY_CREATEDON = "createdon";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_PACKING = "stock";
+    public static final String KEY_SELLINGPRICE = "sellingprice";
+    public static final String KEY_IMG1 = "img1";
+    public static final String KEY_IMG2 = "img2";
+    public static final String KEY_IMG3 = "img3";
+    public static final String KEY_IMG4 = "img4";
+    public static final String KEY_IMG5 = "img5";
+    public static final String KEY_STOCK = "stock";
+
+
+
+
+
+    public static final String JSON_ARRAY = "result";
+}
